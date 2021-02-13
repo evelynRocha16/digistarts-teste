934dd0a5326aee19a97032626c44e3c1a2331deb
Evelyn Rocha <evelyn.tecnologia@outlook.com>
2021-02-13 09:44:52 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'testeDigistarts';
+
+  public formNumeros: FormGroup;
+  public numeros = [];
+  public n: Number;
+
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formNumeros = this.formBuilder.group({
+      num: [this.n]
+    });
+  }
+  
+
+  adicionarArray() {
+    this.numeros.push()
+  }
+
+  adicionar() {
+    let n = this.numeros[0];
+    let variosN = this.numeros.filter(i => i != n);
+
+    variosN.forEach(n => 1<=n && n<=1000);
+
+    let k = variosN.filter((i,p) => variosN.indexOf(i) === p).sort((a,b) => a - b);
+    console.log(k);
+  }
+}
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
